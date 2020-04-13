893af9ccce205a8dff7b145b8dcedc3e11b960d6
uchidataku <bbb.rhcp99@gmail.com>
2020-04-13 07:37:03 +0000

    userテーブルとauthの作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,6 +81,8 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
+                
+                <p>musicbbs</p>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
