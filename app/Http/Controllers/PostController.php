ba87ef397dc5ccc91e101f7ffca524cfcc7e99b4
uchidataku <bbb.rhcp99@gmail.com>
2020-04-17 05:58:38 +0000

    スレッド投稿機能の実装とデバックツールの導入 ・categoryモデルの作成 ・category_postテーブル（中間テーブル）の作成 ・createページの作成 ・デバックツールをインストール ・ログイン後と新規登録後のリダイレクト先をtoppageに変更

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     public function create()
-    {
-        return view('posts.create');
+    {   
+        $categories = \App\Category::all();
+        return view('posts.create')->with('categories',$categories);
     }
     
+    public function store(Request $request)
+    {
+        $user_id = Auth::id();
+        $post = new \App\Post();
+        $post->user_id = $user_id;
+        $post->title = $request->title;
+        $post->text = $request->text;
+        $post->save();
+        $post->categories()->attach(request()->categories);
+        return redirect('/');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -14,4 +14,9 @@ class Post extends Model
     {
         return $this->belongsTo('App\User');
     }
+    
+    public function categories()
+    {
+        return $this->belongsToMany('App\Category');
+    }
 }
